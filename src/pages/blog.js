a61c9de68791bde74cc4970682af962e674dd50c
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2021-01-16 17:06:22 +0100

    premieres modifs test

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -17,7 +17,7 @@ class BlogIndex extends React.Component {
           <Helmet title={siteTitle} />
           <div className={styles.hero}>Blog</div>
           <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
+            <h2 className="section-headline">Articles récents</h2>
             <ul className="article-list">
               {posts.map(({ node }) => {
                 return (
